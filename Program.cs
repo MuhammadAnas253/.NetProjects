ecbe9e365f7cb1ab1129f1386963dc705d22c3ee
Muhammad Anas <160848122+MuhammadAnas253@users.noreply.github.com>
2024-08-31 22:32:07 +0500

    Add files via upload

--- Program.cs
+++ Program.cs
@@ -1,49 +1,72 @@
-using DotNetCoreWebApi.DataContext;
-using Microsoft.EntityFrameworkCore;
-
-var builder = WebApplication.CreateBuilder(args);
-
-static void NewMethod(WebApplicationBuilder builder)
+﻿using System;
+using System.ComponentModel.Design;
+namespace Calculator
 {
-    NewMethod1(builder);
-
-    static void NewMethod1(WebApplicationBuilder builder)
+class Program
     {
-        NewMethod2(builder);
+
+
+
+        static void Main(string[] args)
+        {
+
+            int num1;
+            int num2;
+            int result;
+            string answer;
+
+
+
+            Console.WriteLine("Hello.welcome to the calculator program");
+            Console.WriteLine("Please enter your first number.");
+
+            num1 = Convert.ToInt32(Console.ReadLine());
+            Console.WriteLine("Please enter your second number.");
+
+            num2 = Convert.ToInt32(Console.ReadLine());
+            Console.WriteLine("What type of expression would you like to do ");
+            Console.WriteLine("Please enter a for addition ,s for subtraction , m for multiplication , d for division");
+
+            answer = Console.ReadLine();
+
+
+            if (answer == "a")
+            {
+                result = num1 + num2;
+            }
+
+
+            else if (answer == "s")
+            {
+
+                result = num1 - num2;
+
+            }
+
+            else if (answer == "m")
+            {
+
+                result = num1 * num2;
+
+            }
+            else if (answer == "d")
+            {
+
+                result = num1 / num2;
+
+            }
+            else
+            {
+                Console.WriteLine("Invalid operation selected.");
+                return;
+            }
+
+            Console.WriteLine("Result: " + result);
+
+        }
     }
-}
 
-// Add services to the container.
 
-builder.Services.AddControllers();
-// Learn more about configuring Swagger/OpenAPI at https://aka.ms/aspnetcore/swashbuckle
-builder.Services.AddEndpointsApiExplorer();
-builder.Services.AddSwaggerGen();
 
-NewMethod(builder);
-var app = builder.Build();
 
-// Configure the HTTP request pipeline.
-if (app.Environment.IsDevelopment())
-{
-    app.UseSwagger();
-    app.UseSwaggerUI();
-}
-
-app.UseHttpsRedirection();
-
-app.UseAuthorization();
-
-app.MapControllers();
-
-app.Run();
-
-static void NewMethod2(WebApplicationBuilder builder)
-{
-    NewMethod1(builder);
-}
-
-static void NewMethod1(WebApplicationBuilder builder)
-{
-    builder.Services.AddDbContext<DataBaseContext>(options => options.UseSqlServer(builder.Configuration.GetConnectionString("DefaultConnection")));
 }
